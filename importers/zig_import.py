77f4e353b26893f42d2dfde2d28319d17dcd0fae
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2022-01-12 22:51:06 +0100

    add `const testing = std.testing;`

--- importers/zig_import.py
+++ importers/zig_import.py
@@ -16,15 +16,22 @@ def zig_expand_import(include: str) -> str:
     if not include:
         return '@import("");'
 
+    if "=" in include:
+        # bailout, the user want to type everything themselves
+        return include
+
     name = get_name(include)
     if include.endswith(".zig"):
         return f'const {name} = @import("{include}");'
 
     if "." in include:
-        # If the user has started typing ".zig", complete it for them
         base, suffix = include.rsplit(".", 1)
+        # If the user has started typing ".zig", complete it for them
         if "zig".startswith(suffix):
             return f'const {name} = @import("{base}.zig");'
+
+        # If the user is trying to alias a submodule: "std.testing"
+        return f"const {suffix} = {include};"
 
     # Don't add .zig to the standard packages
     # Also tries to detect the build option package.
@@ -46,21 +53,23 @@ class ZigImporter(importers.Importer):
         return zig_expand_import(include)
 
 
-def zig_samples() -> Dict[str, str]:
-    return {
-        # basic imports
-        "std": 'const std = @import("std");',
-        "builtin": 'const builtin = @import("builtin");',
-        "module.zig": 'const module = @import("module.zig");',
-        # completes ".zig":
-        "module": 'const module = @import("module.zig");',
-        "module.": 'const module = @import("module.zig");',
-        "module.z": 'const module = @import("module.zig");',
-        "module.zi": 'const module = @import("module.zig");',
-
-    }
+ZIG_SAMPLES = {
+    # basic imports
+    "std": 'const std = @import("std");',
+    "builtin": 'const builtin = @import("builtin");',
+    "module.zig": 'const module = @import("module.zig");',
+    # alias a module from std
+    "std.testing": "const testing = std.testing;",
+    # completes ".zig":
+    "module": 'const module = @import("module.zig");',
+    "module.": 'const module = @import("module.zig");',
+    "module.z": 'const module = @import("module.zig");',
+    "module.zi": 'const module = @import("module.zig");',
+    # bailout:
+    "const a =": "const a =",
+}
 
 
 def test_zig_expand_samples() -> None:
-    for base, expanded in zig_samples().items():
+    for base, expanded in ZIG_SAMPLES.items():
         assert expanded == zig_expand_import(base)
